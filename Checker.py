d83e91189c370fff300242404f457188bf9a6f2a
Seven7Up <seven7up@seven7up.org>
2021-04-17 17:08:43 -0400

    Some basic changes but STILL NEED WORK

--- Checker.py
+++ Checker.py
@@ -1,49 +1,60 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 import requests
 from sys import argv
-from os import system
-from colorama import Fore,Style
+from colorama import Fore
+
 
 def check(proxy):
-    headers = {
-        "Accept":"application/json",
-        "User-Agent":"Mozilla/5.0 (X11; Win64 x64; rv:86.0) Gecko/20100101 Firefox/86.0",
-        }
-    r = requests.get("https://httpbin.org/ip", headers=headers, proxies={"http": "http://" + proxy, "https": "https://" + proxy}, timeout=7)
-    print(Fore.LIGHTGREEN_EX + r.json(), ":", r.status_code)
-    file_with_goods = open("good.txt","a")
-    file_with_goods.write(proxy)
+    headersDic = {
+        "Accept": "application/json",
+        "User-Agent": "Mozilla/5.0 (X11; Win64 x64; rv:86.0) Gecko/20100101 Firefox/86.0",
+        "Host": "httpbin.org"
+    }
+    proxyDic = {
+        "http": "http://" + proxy,
+        "https": "https://" + proxy,
+    }
+    r = requests.get("https://httpbin.org/ip",
+                     headers=headersDic, proxies=proxyDic, timeout=10)
+    print(Fore.LIGHTGREEN_EX, r.json(), ":", r.status_code)
+    file_with_goods = open("good.txt", "a")
+    file_with_goods.write(proxy + "\n")
+
 
 def print_help():
-    system("clear")
-    print(Fore.LIGHTCYAN_EX)
-    print("Usage: -f <filename> - Check file with proxies")
-    print("Usage: -p <proxy> - check only one proxy")
-    print("Usage: --help - show this menu")
+    print(Fore.LIGHTRED_EX + "Note: The proxy should be <ip address>:<port>")
+    print(Fore.LIGHTCYAN_EX + "Usage: -f <filename> - Check file with proxies")
+    print(Fore.LIGHTCYAN_EX + "Usage: -p <proxy> - check only one proxy")
+    print(Fore.LIGHTCYAN_EX + "Usage: --help - show this menu")
+
 
 if len(argv) > 1:
-    commands = ["--help","-h","-f","-p","--file","--proxy"]
+    commands = ["--help", "-h", "-f", "--file", "-p", "--proxy"]
     if argv[1] in commands:
-        if argv[1] in ("--help","-h"):
+        if argv[1] in ("--help", "-h"):
             print_help()
-        elif argv[1] in ("-f","--file"):
+        elif argv[1] in ("-f", "--file"):
             file = open(argv[2])
             list = file.readlines()
-            system("clear")
-            print(Fore.LIGHTYELLOW_EX + "Checking " + str(len(list)) + " proxies!")
+            print(Fore.LIGHTYELLOW_EX + "Checking " +
+                  str(len(list)) + " proxies!")
             print("Please Wait :)")
-            for line in range(len(list)):
-                proxy = list[line]
-                try:
-                    check(proxy)
-                except:
+            for index in range(len(list)):
+                if index == "":
                     pass
+                else:
+                    line = list[index]
+                    proxy = line.replace("\n", "")
+                    try:
+                        check(proxy)
+                    except:
+                        print(Fore.LIGHTRED_EX + proxy + " failed!")
+                        pass
             file.close()
             print(Fore.LIGHTGREEN_EX + "GoodBye!")
-            exit()
-        elif argv[1] in ("-p","--proxy"):
-            argv[2] = argv[2].split(" ")[0]
+        elif argv[1] in ("-p", "--proxy"):
+            print(Fore.LIGHTYELLOW_EX + "Checking " + argv[2] + "!")
             try:
                 check(argv[2])
             except:
